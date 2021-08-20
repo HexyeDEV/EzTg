b34a341753165fadecad393fe02aae44de60c7ef
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-20 17:28:46 +0200

    Update main.py

--- EzTg/src/main.py
+++ EzTg/src/main.py
@@ -65,3 +65,17 @@ class EzTg:
     def editMessageText(self, chat_id, message_id, text, inline_message_id=None, parse_mode='Markdown', entities=None, disable_web_page_preview=False, reply_markup=None):
         """bot.edit(chat_id, message_id, text)"""
         return self.send('editMessageText', chat_id=chat_id, message_id=message_id, inline_message_id=inline_message_id, text=text, parse_mode=parse_mode, entities=entities, disable_web_page_preview=disable_web_page_preview, reply_markup=reply_markup)
+
+
+    """User info Methods"""
+
+    """Get user id from message"""
+    def get_author_id(self, message):
+        return message['from']['id']
+    
+
+    """Chat info Methods"""
+
+    """Get chat id from message"""
+    def get_chat_id(self, message):
+        return message['chat']['id']
