7061fa6c93e950a49a5477caa5e4053063d472c5
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-20 19:05:05 +0200

    Update setup.py

--- EzTg/setup.py
+++ EzTg/setup.py
@@ -1,1 +1,27 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EzTg",
+    version="0.0.1",
+    author="Hexye",
+    author_email="dragonsale22@gmail.com",
+    description="An api wrapper for telegram easy to use according to telegram core.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HexyeDEV/EzTg/",
+    download_url="https://github.com/HexyeDEV/EzTg/releases",
+    project_urls={
+        "Bug Tracker": "https://github.com/HexyeDEV/EzTg/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
