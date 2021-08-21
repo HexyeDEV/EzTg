f1bb5d0457b764f91a75af7f90441d494286d145
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-21 14:52:39 +0200

    Update __init__.py

--- EzTg/src/methods/__init__.py
+++ EzTg/src/methods/__init__.py
@@ -1,5 +1,5 @@
-import chat.get_chat_id
-import message.sendMessage
-import message.deleteMessage
-import message.editMessageText
-import user.get_author_id
+fron chat.get_chat_id import get_chat_id
+from message.sendMessage import sendMessage
+from message.deleteMessage import deleteMessage
+from message.editMessageText import editMessageText
+from user.get_author_id import get_author_id
