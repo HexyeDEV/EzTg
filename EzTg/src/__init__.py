857e513ef2bf4e6818e8b1757128396849fa0c0d
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-21 16:15:28 +0200

    Delete __init__.py

--- EzTg/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from methods.user import get_author_id
-from methods.chat import get_chat_id
-from methods.message import editMessageText, sendMessage, deleteMessage
-from main import Parse, TokenError, EzTg
