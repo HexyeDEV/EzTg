56aa9760daab9bd46f5cadad950c2111633a41e3
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-21 15:43:23 +0200

    Delete __init__.py

--- EzTg/src/methods/__init__.py
+++
@@ -1,5 +0,0 @@
-fron chat.get_chat_id import get_chat_id
-from message.sendMessage import sendMessage
-from message.deleteMessage import deleteMessage
-from message.editMessageText import editMessageText
-from user.get_author_id import get_author_id
