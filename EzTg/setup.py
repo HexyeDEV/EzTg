110f883ff5e33255f50c53bb5fa73d0d2148f486
Hexye <dragonsale22@gmail.com>
2022-12-26 21:50:52 +0100

    package-related changes

--- EzTg/setup.py
+++ EzTg/setup.py
@@ -5,7 +5,7 @@ with open("../README.rst", "r") as fh:
 
 setuptools.setup(
     name="EzTg",
-    version="0.1.5",
+    version="0.2.0",
     author="Hexye",
     author_email="dragonsale22@gmail.com",
     description=
