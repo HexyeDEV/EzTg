57a690d4abd347ef21998276f6e9ffc72da2e72f
Restyled.io <commits@restyled.io>
2023-01-11 22:22:56 +0000

    Restyled by reorder-python-imports

--- docs/conf.py
+++ docs/conf.py
@@ -2,10 +2,8 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
 
