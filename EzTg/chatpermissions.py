5dd6daee8bd8df07c9319ec60f0c0887b11fba7c
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2023-03-05 16:21:46 +0100

    changes for #20

--- EzTg/chatpermissions.py
+++ EzTg/chatpermissions.py
@@ -1,6 +1,6 @@
 class ChatPermissions:
     """Describes actions that a non-administrator user is allowed to take in a chat.
-    
+
     Attributes
     ----------
     can_send_messages: `bool`
@@ -30,22 +30,23 @@ class ChatPermissions:
     can_pin_messages: `bool`
         Optional. True, if the user is allowed to pin messages. Ignored in public supergroups.
     can_manage_topics: `bool`
-        Optional. True, if the user is allowed to create forum topics. If omitted defaults to the value of can_pin_messages."""
+        Optional. True, if the user is allowed to create forum topics. If omitted defaults to the value of can_pin_messages.
+    """
 
     def __init__(self, data):
-        self.can_send_messages = data.get('can_send_messages')
-        self.can_send_audios = data.get('can_send_audios')
-        self.can_send_documents = data.get('can_send_documents')
-        self.cabn_send_photos = data.get('cabn_send_photos')
-        self.can_send_videos = data.get('can_send_videos')
-        self.can_send_videos_notes = data.get('can_send_videos_notes')
-        self.can_send_voice_notes = data.get('can_send_voice_notes')
-        self.can_send_polls = data.get('can_send_polls')
-        self.can_send_other_messages = data.get('can_send_other_messages')
-        self.can_add_web_page_previews = data.get('can_add_web_page_previews')
-        self.can_change_info = data.get('can_change_info')
-        self.can_invite_users = data.get('can_invite_users')
-        self.can_pin_messages = data.get('can_pin_messages')
-        self.can_manage_topics = data.get('can_manage_topics')
+        self.can_send_messages = data.get("can_send_messages")
+        self.can_send_audios = data.get("can_send_audios")
+        self.can_send_documents = data.get("can_send_documents")
+        self.cabn_send_photos = data.get("cabn_send_photos")
+        self.can_send_videos = data.get("can_send_videos")
+        self.can_send_videos_notes = data.get("can_send_videos_notes")
+        self.can_send_voice_notes = data.get("can_send_voice_notes")
+        self.can_send_polls = data.get("can_send_polls")
+        self.can_send_other_messages = data.get("can_send_other_messages")
+        self.can_add_web_page_previews = data.get("can_add_web_page_previews")
+        self.can_change_info = data.get("can_change_info")
+        self.can_invite_users = data.get("can_invite_users")
+        self.can_pin_messages = data.get("can_pin_messages")
+        self.can_manage_topics = data.get("can_manage_topics")
         if self.can_manage_topics is None:
             self.can_manage_topics = self.can_pin_messages
