a384d6b96498e213172178fb217991ff82876fda
Restyled.io <commits@restyled.io>
2023-03-05 14:23:50 +0000

    Restyled by autopep8

--- EzTg/chatphoto.py
+++ EzTg/chatphoto.py
@@ -1,6 +1,6 @@
 class ChatPhoto:
     """Represents a chat photo.
-    
+
     Attributes
     ----------
     small_file_id: `str`
@@ -11,7 +11,7 @@ class ChatPhoto:
         File identifier of big (640x640) chat photo. This file_id can be used only for photo download and only for as long as the photo is not changed.
     big_file_unique_id: `str`
         Unique file identifier of big (640x640) chat photo, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file."""
-    
+
     def __init__(self, data):
         self.small_file_id = data['small_file_id']
         self.small_file_unique_id = data['small_file_unique_id']
