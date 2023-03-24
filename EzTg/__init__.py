e9265b82e2b37f1e1ff0fef5576a6a410933b4fd
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2023-03-24 21:10:47 +0100

    testing shi

--- EzTg/__init__.py
+++ EzTg/__init__.py
@@ -1,6 +1,18 @@
 __title__ = "EzTg"
 __author__ = "Hexye"
 
+from .animation import Animation
+from .audio import Audio
+from .chat import Chat
+from .chatlocation import ChatLocation
+from .chatpermissions import ChatPermissions
+from .chatphoto import ChatPhoto
+from .document import Document
 from .inlinekeyboard import InlineKeyboard
+from .location import Location
 from .main import TelegramClient
+from .message import Message
+from .photosize import PhotoSize
 from .replykeyboardmarkup import ReplyKeyboardMarkup
+from .user import User
+from .video import Video
